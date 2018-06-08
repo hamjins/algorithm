90d0cad24e9c4fa14b7a409b80012a19d0a20960
hamjin <coronal95@naver.com>
2018-06-09 04:09:41 +0900

    UKP

--- 9_DynamicProgramming/UKP.cpp
+++ 9_DynamicProgramming/UKP.cpp
@@ -22,9 +22,12 @@ int ans(int n, int W) {
 	int *dp = new int[W + 1];
 	memset(dp, 0, sizeof(dp));
 
+	//무게제한 0부터 W까지
 	for (int i = 0; i <= W; i++) {
-		for (int j = 0; j <= n; j++) {
-			if (j <= i) dp[i] = max(dp[i], dp[i - j] + v[j]);
+		//0번 짐부터 n번 짐까지 순회하면서 최대 가치 찾기
+		for (int w = 0; w <= n; w++) {
+			//가방의 무게제한이 i번 짐의 무게보다 크다면, i번 짐을 챙겼을 때의 최대가치 계산
+			if (w <= i) dp[i] = max(dp[i], dp[i - w] + v[w]);
 		}
 	}
 	return dp[W];
